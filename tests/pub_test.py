19301cf4f25bfc66434aebb427921127c9511944
Bela <belahamid@gmail.com>
2020-11-25 10:27:01 +0000

    initial commit

---
+++ tests/pub_test.py
@@ -0,0 +1,11 @@
+import unittest
+from src.pub import Pub #capital P because class names always begin with capital letter
+
+class TestPub(unittest.TestCase):#these parameters tell python this is a test, and not a normal class
+    def setUp(self):
+        self.pub = Pub("The Prancing Pony", 100.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("The Prancing Pony", self.pub.name)
+
+    
